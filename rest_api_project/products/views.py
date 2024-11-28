e6da4222375916e348647a8f560cdf334f2ceea7
CynthiaWKamau <cynthia.kamau@strathmore.edu>
2024-11-28 14:54:21 +0300

    addition of the script, urls, views

--- rest_api_project/products/views.py
+++ rest_api_project/products/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
 
-import json
+@api_view(['GET', 'POST'])
+def product_list(request):
+    if request.method == 'GET':
+        # Get all products from the database
+        products = Product.objects.all()
+        # Serialize the products into JSON format
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
 
+    elif request.method == 'POST':
+        # Create a new product
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()  # Save the new product to the database
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
